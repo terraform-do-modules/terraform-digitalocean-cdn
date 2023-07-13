b068ff895de70960c3a12849bd3c371f85a19143
mahesh yadav <devopsmahesh.it@gmail.com>
2023-07-13 20:02:42 +0530

    fix: add resources and update github action

--- _examples/complete/main.tf
+++ _examples/complete/main.tf
@@ -1,4 +1,28 @@
-# ------------------------------------------------------------------------------
-# Resources
-# ------------------------------------------------------------------------------
-locals {}
+provider "digitalocean" {
+  spaces_access_id  = "XXXXXXXXXXXX"
+  spaces_secret_key = "XXXXXXXXXXXX"
+}
+
+##------------------------------------------------
+## spaces module call
+##------------------------------------------------
+module "spaces" {
+  source        = "git::https://github.com/terraform-do-modules/terraform-digitalocean-spaces.git?ref=internal-424"
+  name          = "spaces"
+  environment   = "test"
+  acl           = "private"
+  force_destroy = false
+  region        = "nyc3"
+
+}
+##------------------------------------------------
+## cdn module call
+##------------------------------------------------
+module "cdn" {
+  depends_on       = [module.spaces]
+  source           = "./../../"
+  origin           = module.spaces.bucket_domain_name
+  ttl              = 3600
+  custom_domain    = ""
+  certificate_name = ""
+}
--- variables.tf
+++ variables.tf
@@ -1,8 +1,38 @@
 # ------------------------------------------------------------------------------
 # Variables
 # ------------------------------------------------------------------------------
-variable "label_order" {
-  type        = list(string)
-  default     = ["name", "environment"]
-  description = "Label order, e.g. `name`,`environment`."
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Whether to create the resources. Set to `false` to prevent the module from creating any resources."
+}
+
+variable "origin" {
+  type        = string
+  default     = ""
+  description = "The fully qualified domain name, (FQDN) for a Space."
+}
+
+variable "ttl" {
+  type        = number
+  default     = 3600
+  description = "The time to live for the CDN Endpoint, in seconds. Default is 3600 seconds."
+}
+
+variable "certificate_name" {
+  type        = string
+  default     = null
+  description = "The unique name of a DigitalOcean managed TLS certificate used for SSL when a custom subdomain is provided."
+}
+
+variable "certificate_id" {
+  type        = string
+  default     = null
+  description = "Deprecated The ID of a DigitalOcean managed TLS certificate used for SSL when a custom subdomain is provided."
+}
+
+variable "custom_domain" {
+  type        = string
+  default     = null
+  description = "The fully qualified domain name (FQDN) of the custom subdomain used with the CDN Endpoint."
 }
